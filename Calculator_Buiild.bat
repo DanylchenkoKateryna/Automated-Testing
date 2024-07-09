07173691ed9e38894d6036897a52b51b86290126
AzureAD\KaterynaDanylchenko <kada@opinov8.com>
2024-07-09 16:14:20 +0300

    update

--- Calculator_Buiild.bat
+++ Calculator_Buiild.bat
@@ -2,6 +2,6 @@ msbuild Calculator/AnalizerClassLibrary/AnalizerClassLibrary.csproj /p:Configura
 msbuild Calculator/CalcClassBr/CalcClassBr.csproj /p:Configuration=Release
 msbuild Calculator/ErrorLibrary/ErrorLibrary.csproj /p:Configuration=Release
 msbuild Calculator/GraphInterface/GraphInterface.csproj /p:Configuration=Release
-msbuild Calculator/Tests/AnalizerClassLibrary.Tests.csproj /p:Configuration=Release
+msbuild Calculator/AnalizerClassLibrary.Tests/AnalizerClassLibrary.Tests.csproj /p:Configuration=Release
 msbuild Calculator/xUnitProject/xUnitProject.csproj /p:Configuration=Release
 pause
