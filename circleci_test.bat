b33d318f82d24ec94bcb8184011eb9b8b7c05c4e
AzureAD\KaterynaDanylchenko <kada@opinov8.com>
2024-07-09 22:40:50 +0300

    update

--- circleci_test.bat
+++ circleci_test.bat
@@ -1,4 +1,4 @@
-dotnet test Calculator/xUnitProject/xUnitProject.csproj --configuration Release --logger "junit;LogFileName=xunit-test-results.xml" 
-dotnet test Calculator/AnalizerClassLibrary.Tests/AnalizerClassLibrary.Tests.csproj --configuration Release --no-build --logger "junit;LogFileName=analizer-test-results.xml" 
+dotnet test Calculator/xUnitProject/xUnitProject.csproj --configuration Release --logger "trx;LogFileName=xunit-test-results.xml" 
+dotnet test Calculator/AnalizerClassLibrary.Tests/AnalizerClassLibrary.Tests.csproj --configuration Release --no-build --logger "trx;LogFileName=analizer-test-results.xml" 
 
 pause
