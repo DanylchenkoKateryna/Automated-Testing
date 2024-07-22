ce4b4f2d94cdd4f37b4e9ecc2973f1af473ce98c
AzureAD\KaterynaDanylchenko <kada@opinov8.com>
2024-07-22 11:06:27 +0300

    update config.yml

--- circleci_test.bat
+++ circleci_test.bat
@@ -1,4 +1,5 @@
 dotnet test Calculator/xUnitProject/xUnitProject.csproj --configuration Release --logger "junit;LogFileName=xunit-test-results.xml" 
 dotnet test Calculator/AnalizerClassLibrary.Tests/AnalizerClassLibrary.Tests.csproj --configuration Release --no-build --logger "junit;LogFileName=analizer-test-results.xml" 
-
+dotnet test Calculator/TestProject2.Tests/TestProject2.Tests.csproj --configuration Release --logger "junit;LogFileName=xunit2-test-results.xml" 
+dotnet test Calculator/TestProject2.Tests/TestProject2.Tests.csproj --configuration Release --logger "junit;LogFileName=xunit3-test-results.xml" 
 pause
