a2ede10a2719c3730cc4c12f0572319aff7f07c2
DanylchenkoKateryna <112478961+DanylchenkoKateryna@users.noreply.github.com>
2024-07-22 11:17:41 +0300

    Update circleci_test.bat

--- circleci_test.bat
+++ circleci_test.bat
@@ -1,5 +1,5 @@
-dotnet test Calculator/xUnitProject/xUnitProject.csproj --configuration Release --logger "junit;LogFileName=xunit-test-results.xml" 
-dotnet test Calculator/AnalizerClassLibrary.Tests/AnalizerClassLibrary.Tests.csproj --configuration Release --no-build --logger "junit;LogFileName=analizer-test-results.xml" 
-dotnet test Calculator/TestProject2.Tests/TestProject2.Tests.csproj --configuration Release --logger "junit;LogFileName=xunit2-test-results.xml" 
-dotnet test Calculator/TestProject2.Tests/TestProject2.Tests.csproj --configuration Release --logger "junit;LogFileName=xunit3-test-results.xml" 
+dotnet test Calculator/xUnitProject/xUnitProject.csproj --configuration Release --logger "junit;LogFileName=TestResults/xunit-test-results.xml" 
+dotnet test Calculator/AnalizerClassLibrary.Tests/AnalizerClassLibrary.Tests.csproj --configuration Release --no-build --logger "junit;LogFileName=TestResults/analizer-test-results.xml" 
+dotnet test Calculator/TestProject2.Tests/TestProject2.Tests.csproj --configuration Release --logger "junit;LogFileName=TestResults/xunit2-test-results.xml" 
+dotnet test Calculator/TestProject2.Tests/TestProject2.Tests.csproj --configuration Release --logger "junit;LogFileName=TestResults/xunit3-test-results.xml" 
 pause
