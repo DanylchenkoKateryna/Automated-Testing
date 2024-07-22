8313461df5646e4a6d5dcd34731cdb28233c1f10
DanylchenkoKateryna <112478961+DanylchenkoKateryna@users.noreply.github.com>
2024-07-22 11:51:12 +0300

    Update circleci_test.bat

--- circleci_test.bat
+++ circleci_test.bat
@@ -1,5 +1,5 @@
-dotnet test Calculator/xUnitProject/xUnitProject.csproj --configuration Release --logger "junit;LogFileName=Calculator/TestResults/test-results.xml" 
-dotnet test Calculator/AnalizerClassLibrary.Tests/AnalizerClassLibrary.Tests.csproj --configuration Release --no-build --logger "junit;LogFileName=Calculator/TestResults/test-results.xml" 
-dotnet test Calculator/TestProject2.Tests/TestProject2.Tests.csproj --configuration Release --logger "junit;LogFileName=Calculator/TestResults/test-results.xml" 
-dotnet test Calculator/TestProject2.Tests/TestProject2.Tests.csproj --configuration Release --logger "junit;LogFileName=Calculator/TestResults/test-results.xml" 
+dotnet test Calculator/xUnitProject/xUnitProject.csproj --configuration Release --logger "junit;LogFileName=Calculator/TestResults/test-results-1.xml" 
+dotnet test Calculator/AnalizerClassLibrary.Tests/AnalizerClassLibrary.Tests.csproj --configuration Release --no-build --logger "junit;LogFileName=Calculator/TestResults/test-results-2.xml" 
+dotnet test Calculator/TestProject2.Tests/TestProject2.Tests.csproj --configuration Release --logger "junit;LogFileName=Calculator/TestResults/test-results-3.xml" 
+dotnet test Calculator/TestProject2.Tests/TestProject2.Tests.csproj --configuration Release --logger "junit;LogFileName=Calculator/TestResults/test-results-4.xml" 
 pause
