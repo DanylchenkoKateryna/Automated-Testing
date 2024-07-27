f465c6c41f703d575bcdbd3b38cbc9307aa305b3
AzureAD\KaterynaDanylchenko <kada@opinov8.com>
2024-07-27 20:18:05 +0300

    update tests

--- Calculator/TestProject2.Tests/UnitTest1.cs
+++ Calculator/TestProject2.Tests/UnitTest1.cs
@@ -43,5 +43,25 @@ namespace TestProject2.Tests
             int result = calcClass.Add(a, b);
             Assert.Equal(expected, result);
         }
+
+        [Theory]
+        [InlineData(5, 3, 2)]
+        [InlineData(9, 9, 0)]
+        [InlineData(15, 11, 4)]
+        public void Sub_VariousInputs_ReturnsExpectedResults(int a, int b, int expected)
+        {
+            int result = calcClass.Sub(a, b);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(10, 2, 5)]
+        [InlineData(10, 5, 2)]
+        [InlineData(15, 5, 3)]
+        public void Div_VariousInputs_ReturnsExpectedResults(int a, int b, int expected)
+        {
+            int result = calcClass.Div(a, b);
+            Assert.Equal(expected, result);
+        }
     }
 }
