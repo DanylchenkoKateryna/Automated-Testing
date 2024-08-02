3c4e81b33f57f7cedbec5bbe7dfbaacd65999026
DanylchenkoKateryna <112478961+DanylchenkoKateryna@users.noreply.github.com>
2024-08-02 22:25:15 +0300

    Update Calculator_Buiild.bat

--- .circleci/Calculator_Buiild.bat
+++ .circleci/Calculator_Buiild.bat
@@ -3,7 +3,7 @@ msbuild Calculator/CalcClassBr/CalcClassBr.csproj /p:Configuration=Release
 msbuild Calculator/ErrorLibrary/ErrorLibrary.csproj /p:Configuration=Release
 msbuild Calculator/GraphInterface/GraphInterface.csproj /p:Configuration=Release
 msbuild Calculator/AnalizerClassLibrary.Tests/AnalizerClassLibrary.Tests.csproj /p:Configuration=Release
-msbuild Calculator/xUnitProject/xUnitProject.csproj /p:Configuration=Release /t:Restore
+msbuild Calculator/xUnitProject/xUnitProject.csproj /p:Configuration=Release
 msbuild Calculator/TestProject2.Tests/TestProject2.Tests.csproj /p:Configuration=Release
 msbuild Calculator/TestProject3.Tests/TestProject3.Tests.csproj /p:Configuration=Release
 pause
