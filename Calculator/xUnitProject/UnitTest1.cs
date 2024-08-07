92eea1cbe11d727627f8c40f08a6b1360cc1b106
DanylchenkoKateryna <112478961+DanylchenkoKateryna@users.noreply.github.com>
2024-08-07 13:30:06 +0300

    Update UnitTest1.cs

--- Calculator/xUnitProject/UnitTest1.cs
+++ Calculator/xUnitProject/UnitTest1.cs
@@ -11,17 +11,17 @@ namespace xUnitProject
         private readonly CalcClass calcClass = new CalcClass();
         private readonly Cache cache = HttpRuntime.Cache;
 
-        [Theory]
-        [InlineData(2, 2, 4)]
-        [InlineData(2, 3, 6)]
-        [InlineData(1, 9, 9)]
-        [InlineData(10, 0, 0)]
-        public void Mult_VariousInputs_ReturnsExpectedResults(int a, int b, int expected)
-        {
-            int result = calcClass.Mult(a, b);
-            Assert.Equal(expected, result);
-            result.Should().Be(expected);
-        }
+        //[Theory]
+        //[InlineData(2, 2, 4)]
+        //[InlineData(2, 3, 6)]
+        //[InlineData(1, 9, 9)]
+        //[InlineData(10, 0, 0)]
+        //public void Mult_VariousInputs_ReturnsExpectedResults(int a, int b, int expected)
+        //{
+        //    int result = calcClass.Mult(a, b);
+        //    Assert.Equal(expected, result);
+        //    result.Should().Be(expected);
+        //}
 
     }
 }
