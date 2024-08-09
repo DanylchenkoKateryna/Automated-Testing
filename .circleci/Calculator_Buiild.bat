9760f590f8655d630128998583cca9eb2a7efc3b
DanylchenkoKateryna <112478961+DanylchenkoKateryna@users.noreply.github.com>
2024-08-09 17:50:05 +0300

    Update Calculator_Buiild.bat

--- .circleci/Calculator_Buiild.bat
+++ .circleci/Calculator_Buiild.bat
@@ -8,19 +8,19 @@ if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 msbuild Calculator/ErrorLibrary/ErrorLibrary.csproj /p:Configuration=Release
 if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
-msbuild Calculator/GraphInterface/GraphInterface.csproj /p:Configuration=Release
+msbuild Calculator/GraphInterface/GraphInterface.csproj /p:Configuration=Release 
 if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
-msbuild Calculator/AnalizerClassLibrary.Tests/AnalizerClassLibrary.Tests.csproj /p:Configuration=Release
+msbuild Calculator/AnalizerClassLibrary.Tests/AnalizerClassLibrary.Tests.csproj /p:Configuration=Release >> build_output.txt
 if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
-msbuild Calculator/xUnitProject/xUnitProject.csproj /p:Configuration=Release
+msbuild Calculator/xUnitProject/xUnitProject.csproj /p:Configuration=Release >> build_output.txt
 if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
-msbuild Calculator/TestProject2.Tests/TestProject2.Tests.csproj /p:Configuration=Release
+msbuild Calculator/TestProject2.Tests/TestProject2.Tests.csproj /p:Configuration=Release >> build_output.txt
 if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
-msbuild Calculator/TestProject3.Tests/TestProject3.Tests.csproj /p:Configuration=Release
+msbuild Calculator/TestProject3.Tests/TestProject3.Tests.csproj /p:Configuration=Release >> build_output.txt
 if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
 pause
