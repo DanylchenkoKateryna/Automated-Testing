8da34d226f304f21e2be4ef4645ea9d879f1176a
DanylchenkoKateryna <112478961+DanylchenkoKateryna@users.noreply.github.com>
2024-08-10 00:42:49 +0300

    Create circleci_restore.bat

---
+++ .circleci/circleci_restore.bat
@@ -0,0 +1,8 @@
+nuget restore Calculator/CalcClassBr/CalcClassBr.csproj
+nuget restore Calculator/AnalizerClassLibrary/AnalizerClassLibrary.csproj
+nuget restore Calculator/ErrorLibrary/ErrorLibrary.csproj
+nuget restore Calculator/GraphInterface/GraphInterface.csproj
+nuget restore Calculator/AnalizerClassLibrary.Tests/AnalizerClassLibrary.Tests.csproj
+nuget restore Calculator/xUnitProject/xUnitProject.csproj
+nuget restore Calculator/TestProject2.Tests/TestProject2.Tests.csproj
+nuget restore Calculator/TestProject3.Tests/TestProject3.Tests.csproj
