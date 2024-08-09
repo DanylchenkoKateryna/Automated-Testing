aa769da34c86b4f9834c76bee6c5e65387fbbe4e
DanylchenkoKateryna <112478961+DanylchenkoKateryna@users.noreply.github.com>
2024-08-09 17:12:01 +0300

    Update Calculator_Buiild.bat

--- .circleci/Calculator_Buiild.bat
+++ .circleci/Calculator_Buiild.bat
@@ -1,9 +1,18 @@
-msbuild Calculator/AnalizerClassLibrary/AnalizerClassLibrary.csproj /p:Configuration=Release
-msbuild Calculator/CalcClassBr/CalcClassBr.csproj /p:Configuration=Release
-msbuild Calculator/ErrorLibrary/ErrorLibrary.csproj /p:Configuration=Release
-msbuild Calculator/GraphInterface/GraphInterface.csproj /p:Configuration=Release
-msbuild Calculator/AnalizerClassLibrary.Tests/AnalizerClassLibrary.Tests.csproj /p:Configuration=Release
-msbuild Calculator/xUnitProject/xUnitProject.csproj /p:Configuration=Release
-msbuild Calculator/TestProject2.Tests/TestProject2.Tests.csproj /p:Configuration=Release
-msbuild Calculator/TestProject3.Tests/TestProject3.Tests.csproj /p:Configuration=Release
+msbuild Calculator/AnalizerClassLibrary/AnalizerClassLibrary.csproj /p:Configuration=Debug
+if (-not $?) { exit 1 }
+msbuild Calculator/CalcClassBr/CalcClassBr.csproj /p:Configuration=Debug
+if (-not $?) { exit 1 }
+msbuild Calculator/ErrorLibrary/ErrorLibrary.csproj /p:Configuration=Debug
+if (-not $?) { exit 1 }
+msbuild Calculator/GraphInterface/GraphInterface.csproj /p:Configuration=Debug
+if (-not $?) { exit 1 }
+msbuild Calculator/AnalizerClassLibrary.Tests/AnalizerClassLibrary.Tests.csproj /p:Configuration=Debug
+if (-not $?) { exit 1 }
+msbuild Calculator/xUnitProject/xUnitProject.csproj /p:Configuration=Debug
+if (-not $?) { exit 1 }
+msbuild Calculator/TestProject2.Tests/TestProject2.Tests.csproj /p:Configuration=Debug
+if (-not $?) { exit 1 }
+msbuild Calculator/TestProject3.Tests/TestProject3.Tests.csproj /p:Configuration=Debug
+if (-not $?) { exit 1 }
+
 pause
