0232056e7271dda227256491ab6433a1dee1947b
DanylchenkoKateryna <112478961+DanylchenkoKateryna@users.noreply.github.com>
2024-08-09 17:20:01 +0300

    Update Calculator_Buiild.bat

--- .circleci/Calculator_Buiild.bat
+++ .circleci/Calculator_Buiild.bat
@@ -1,26 +1,26 @@
 @echo off
 msbuild Calculator/AnalizerClassLibrary/AnalizerClassLibrary.csproj /p:Configuration=Debug
-if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
 
 msbuild Calculator/CalcClassBr/CalcClassBr.csproj /p:Configuration=Debug
-if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
 
 msbuild Calculator/ErrorLibrary/ErrorLibrary.csproj /p:Configuration=Debug
-if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
 
 msbuild Calculator/GraphInterface/GraphInterface.csproj /p:Configuration=Debug
-if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
 
 msbuild Calculator/AnalizerClassLibrary.Tests/AnalizerClassLibrary.Tests.csproj /p:Configuration=Debug
-if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
 
 msbuild Calculator/xUnitProject/xUnitProject.csproj /p:Configuration=Debug
-if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
 
 msbuild Calculator/TestProject2.Tests/TestProject2.Tests.csproj /p:Configuration=Debug
-if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
 
 msbuild Calculator/TestProject3.Tests/TestProject3.Tests.csproj /p:Configuration=Debug
-if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
 
 pause
