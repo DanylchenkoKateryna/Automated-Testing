94bc84d1178a334848967f998fe703d5e9a65a74
DanylchenkoKateryna <112478961+DanylchenkoKateryna@users.noreply.github.com>
2024-08-09 17:17:43 +0300

    Update Calculator_Buiild.bat

--- .circleci/Calculator_Buiild.bat
+++ .circleci/Calculator_Buiild.bat
@@ -1,18 +1,25 @@
 msbuild Calculator/AnalizerClassLibrary/AnalizerClassLibrary.csproj /p:Configuration=Debug
-if (-not $?) { exit 1 }
+if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
 msbuild Calculator/CalcClassBr/CalcClassBr.csproj /p:Configuration=Debug
-if (-not $?) { exit 1 }
+if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
 msbuild Calculator/ErrorLibrary/ErrorLibrary.csproj /p:Configuration=Debug
-if (-not $?) { exit 1 }
+if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
 msbuild Calculator/GraphInterface/GraphInterface.csproj /p:Configuration=Debug
-if (-not $?) { exit 1 }
+if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
 msbuild Calculator/AnalizerClassLibrary.Tests/AnalizerClassLibrary.Tests.csproj /p:Configuration=Debug
-if (-not $?) { exit 1 }
+if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
 msbuild Calculator/xUnitProject/xUnitProject.csproj /p:Configuration=Debug
-if (-not $?) { exit 1 }
+if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
 msbuild Calculator/TestProject2.Tests/TestProject2.Tests.csproj /p:Configuration=Debug
-if (-not $?) { exit 1 }
+if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
 msbuild Calculator/TestProject3.Tests/TestProject3.Tests.csproj /p:Configuration=Debug
-if (-not $?) { exit 1 }
+if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
 pause
