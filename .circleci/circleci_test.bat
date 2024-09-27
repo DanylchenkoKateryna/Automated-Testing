36413186987be623e19268d1103674edc850c4d4
DanylchenkoKateryna <112478961+DanylchenkoKateryna@users.noreply.github.com>
2024-09-27 12:56:17 +0300

    Update circleci_test.bat

--- .circleci/circleci_test.bat
+++ .circleci/circleci_test.bat
@@ -1,9 +1,11 @@
 @echo off
 dotnet test Calculator\xUnitProject\xUnitProject.csproj --configuration Release --no-build --logger "junit;LogFileName=C:\Users\circleci\project\Calculator\TestResults\test-results-1.xml" --collect:"XPlat Code Coverage" --results-directory C:/Users/circleci/project/Calculator/CodeCoverageResults
-IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
 dotnet test Calculator\TestProject2.Tests\TestProject2.Tests.csproj --configuration Release --no-build --logger "junit;LogFileName=C:\Users\circleci\project\Calculator\TestResults\test-results-3.xml" --collect:"XPlat Code Coverage" --results-directory C:/Users/circleci/project/Calculator/CodeCoverageResults
-IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
 dotnet test Calculator\TestProject3.Tests\TestProject3.Tests.csproj --configuration Release --no-build --logger "junit;LogFileName=C:\Users\circleci\project\Calculator\TestResults\test-results-4.xml" --collect:"XPlat Code Coverage" --results-directory C:/Users/circleci/project/Calculator/CodeCoverageResults
-IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
 pause
