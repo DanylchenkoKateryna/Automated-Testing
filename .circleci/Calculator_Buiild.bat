47abc810622bc424839d04e131e2eba2b82827a4
DanylchenkoKateryna <112478961+DanylchenkoKateryna@users.noreply.github.com>
2025-04-15 15:30:40 +0300

    Update Calculator_Buiild.bat

--- .circleci/Calculator_Buiild.bat
+++ .circleci/Calculator_Buiild.bat
@@ -1,11 +1,10 @@
 @echo off
-start msbuild Calculator/AnalizerClassLibrary/AnalizerClassLibrary.csproj /p:Configuration=Release
-start msbuild Calculator/CalcClassBr/CalcClassBr.csproj /p:Configuration=Release
-start msbuild Calculator/ErrorLibrary/ErrorLibrary.csproj /p:Configuration=Release
-start msbuild Calculator/GraphInterface/GraphInterface.csproj /p:Configuration=Release 
-start msbuild Calculator/AnalizerClassLibrary.Tests/AnalizerClassLibrary.Tests.csproj /p:Configuration=Release >> build_output.txt
-start msbuild Calculator/xUnitProject/xUnitProject.csproj /p:Configuration=Release >> build_output.txt
-start msbuild Calculator/TestProject2.Tests/TestProject2.Tests.csproj /p:Configuration=Release >> build_output.txt
-start msbuild Calculator/TestProject3.Tests/TestProject3.Tests.csproj /p:Configuration=Release >> build_output.txt
-
+msbuild Calculator/AnalizerClassLibrary/AnalizerClassLibrary.csproj /p:Configuration=Release /m
+msbuild Calculator/CalcClassBr/CalcClassBr.csproj /p:Configuration=Release /m
+msbuild Calculator/ErrorLibrary/ErrorLibrary.csproj /p:Configuration=Release /m
+msbuild Calculator/GraphInterface/GraphInterface.csproj /p:Configuration=Release /m
+msbuild Calculator/AnalizerClassLibrary.Tests/AnalizerClassLibrary.Tests.csproj /p:Configuration=Release /m 
+msbuild Calculator/xUnitProject/xUnitProject.csproj /p:Configuration=Release /m
+msbuild Calculator/TestProject2.Tests/TestProject2.Tests.csproj /p:Configuration=Release /m
+msbuild Calculator/TestProject3.Tests/TestProject3.Tests.csproj /p:Configuration=Release /m
 pause
