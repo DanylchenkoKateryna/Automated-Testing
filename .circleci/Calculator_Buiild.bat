c5f3ac2cdc264e2f70f3961706e8c6d4109c2323
DanylchenkoKateryna <112478961+DanylchenkoKateryna@users.noreply.github.com>
2025-04-15 15:25:09 +0300

    Update Calculator_Buiild.bat

--- .circleci/Calculator_Buiild.bat
+++ .circleci/Calculator_Buiild.bat
@@ -1,26 +1,11 @@
 @echo off
-msbuild Calculator/AnalizerClassLibrary/AnalizerClassLibrary.csproj /p:Configuration=Release
-if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
-
-msbuild Calculator/CalcClassBr/CalcClassBr.csproj /p:Configuration=Release
-if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
-
-msbuild Calculator/ErrorLibrary/ErrorLibrary.csproj /p:Configuration=Release
-if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
-
-msbuild Calculator/GraphInterface/GraphInterface.csproj /p:Configuration=Release 
-if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
-
-msbuild Calculator/AnalizerClassLibrary.Tests/AnalizerClassLibrary.Tests.csproj /p:Configuration=Release >> build_output.txt
-if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
-
-msbuild Calculator/xUnitProject/xUnitProject.csproj /p:Configuration=Release >> build_output.txt
-if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
-
-msbuild Calculator/TestProject2.Tests/TestProject2.Tests.csproj /p:Configuration=Release >> build_output.txt
-if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
-
-msbuild Calculator/TestProject3.Tests/TestProject3.Tests.csproj /p:Configuration=Release >> build_output.txt
-if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+start msbuild Calculator/AnalizerClassLibrary/AnalizerClassLibrary.csproj /p:Configuration=Release
+start msbuild Calculator/CalcClassBr/CalcClassBr.csproj /p:Configuration=Release
+start msbuild Calculator/ErrorLibrary/ErrorLibrary.csproj /p:Configuration=Release
+start msbuild Calculator/GraphInterface/GraphInterface.csproj /p:Configuration=Release 
+start msbuild Calculator/AnalizerClassLibrary.Tests/AnalizerClassLibrary.Tests.csproj /p:Configuration=Release >> build_output.txt
+start msbuild Calculator/xUnitProject/xUnitProject.csproj /p:Configuration=Release >> build_output.txt
+start msbuild Calculator/TestProject2.Tests/TestProject2.Tests.csproj /p:Configuration=Release >> build_output.txt
+start msbuild Calculator/TestProject3.Tests/TestProject3.Tests.csproj /p:Configuration=Release >> build_output.txt
 
 pause
